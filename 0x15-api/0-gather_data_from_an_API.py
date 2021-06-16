b55ddd47e733db8eacae2a7f7b5369acad470a12
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-16 20:09:50 +0000

    trying to fix last check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,12 +17,9 @@ def get_employee_tasks(employeeId):
     todosRes = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".
         format(employeeId))
-#    print("userRes: {}\n".format(userRes))
-#    print("todoRes: {}\n".format(todoRes))
 
     # get the json from responses
     name = usersRes.json().get('name')
-    # print('name: {}'.format(name))
     todosJson = todosRes.json()
     # save the employee Name
 
@@ -33,7 +30,6 @@ def get_employee_tasks(employeeId):
             completed_counter += 1
             # save the task title to task_list
             task_list.append(task.get('title'))
-    # print('task: {}'.format(task_list)
 
     # print first line
     print('Employee {} is done with task({}/{}):'.format(
@@ -41,14 +37,6 @@ def get_employee_tasks(employeeId):
     # loop the task_list and print tasks
     for title in task_list:
         print('\t {}'.format(title))
-    return 0
 
 if __name__ == '__main__':
     get_employee_tasks(sys.argv[1])
-
-# NOTES
-# use sys to get input from teh terminal
-# need to use at the API provided
-# - name, # tasks done and not, task title
-# tab and space
-# look at requests module
